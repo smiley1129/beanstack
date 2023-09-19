8fd8d8f8873989a97fab2eee9fe81d702150dbf8
Jenish Revaldo <129158780+JenishRevaldo@users.noreply.github.com>
2023-09-19 15:26:08 +0530

    Update application.py

--- application.py
+++ application.py
@@ -1,67 +1,152 @@
-from flask import Flask
-from flask import render_template
-import socket
-import random
-import os
-import argparse
+import logging.handlers
 
-app = Flask(__name__)
+# Create logger
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
 
-color_codes = {
-    "red": "#e74c3c",
-    "green": "#16a085",
-    "blue": "#2980b9",
-    "blue2": "#30336b",
-    "pink": "#be2edd",
-    "darkblue": "#130f40"
-}
+# Handler
+LOG_FILE = '/tmp/sample-app.log'
+handler = logging.handlers.RotatingFileHandler(LOG_FILE, maxBytes=1048576, backupCount=5)
+handler.setLevel(logging.INFO)
 
-SUPPORTED_COLORS = ",".join(color_codes.keys())
+# Formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-# Get color from Environment variable
-COLOR_FROM_ENV = os.environ.get('APP_COLOR')
-# Generate a random color
-COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink"])
+# Add Formatter to Handler
+handler.setFormatter(formatter)
+
+# add Handler to Logger
+logger.addHandler(handler)
+
+welcome = """
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+  <!--
+    Copyright 2012 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+
+    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+
+        http://aws.Amazon/apache2.0/
+
+    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
+  -->
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <title>Welcome</title>
+  <style>
+  body {
+    color: #ffffff;
+    background-color: #E0E0E0;
+    font-family: Arial, sans-serif;
+    font-size:14px;
+    -moz-transition-property: text-shadow;
+    -moz-transition-duration: 4s;
+    -webkit-transition-property: text-shadow;
+    -webkit-transition-duration: 4s;
+    text-shadow: none;
+  }
+  body.blurry {
+    -moz-transition-property: text-shadow;
+    -moz-transition-duration: 4s;
+    -webkit-transition-property: text-shadow;
+    -webkit-transition-duration: 4s;
+    text-shadow: #fff 0px 0px 25px;
+  }
+  a {
+    color: #0188cc;
+  }
+  .textColumn, .linksColumn {
+    padding: 2em;
+  }
+  .textColumn {
+    position: absolute;
+    top: 0px;
+    right: 50%;
+    bottom: 0px;
+    left: 0px;
+
+    text-align: right;
+    padding-top: 11em;
+    background-color: #1BA86D;
+    background-image: -moz-radial-gradient(left top, circle, #6AF9BD 0%, #00B386 60%);
+    background-image: -webkit-gradient(radial, 0 0, 1, 0 0, 500, from(#6AF9BD), to(#00B386));
+  }
+  .textColumn p {
+    width: 75%;
+    float:right;
+  }
+  .linksColumn {
+    position: absolute;
+    top:0px;
+    right: 0px;
+    bottom: 0px;
+    left: 50%;
+
+    background-color: #E0E0E0;
+  }
+
+  h1 {
+    font-size: 500%;
+    font-weight: normal;
+    margin-bottom: 0em;
+  }
+  h2 {
+    font-size: 200%;
+    font-weight: normal;
+    margin-bottom: 0em;
+  }
+  ul {
+    padding-left: 1em;
+    margin: 0px;
+  }
+  li {
+    margin: 1em 0em;
+  }
+  </style>
+</head>
+<body id="sample">
+  <div class="textColumn">
+    <h1>Congratulations</h1>
+    <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
+    <p>This environment is launched with Elastic Beanstalk Python Platform</p>
+  </div>
+ 
+  <div class="linksColumn">
+    <h2>What's Next?</h2>
+    <ul>
+    <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/">AWS Elastic Beanstalk overview</a></li>
+    <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/index.html?concepts.html">AWS Elastic Beanstalk concepts</a></li>
+    <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/create_deploy_Python_django.html">Deploy a Django Application to AWS Elastic Beanstalk</a></li>
+    <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/create_deploy_Python_flask.html">Deploy a Flask Application to AWS Elastic Beanstalk</a></li>
+    <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/create_deploy_Python_custom_container.html">Customizing and Configuring a Python Container</a></li>
+    <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/using-features.loggingS3.title.html">Working with Logs</a></li>
+
+    </ul>
+  </div>
+</body>
+</html>
+"""
 
 
-@app.route("/")
-def main():
-    # return 'Hello'
-    return render_template('hello.html', name=socket.gethostname(), color=color_codes[COLOR])
-
-
-if __name__ == "__main__":
-
-    print(" This is a sample web application that displays a colored background. \n"
-          " A color can be specified in two ways. \n"
-          "\n"
-          " 1. As a command line argument with --color as the argument. Accepts one of " + SUPPORTED_COLORS + " \n"
-          " 2. As an Environment variable APP_COLOR. Accepts one of " + SUPPORTED_COLORS + " \n"
-          " 3. If none of the above then a random color is picked from the above list. \n"
-          " Note: Command line argument precedes over environment variable.\n"
-          "\n"
-          "")
-
-    # Check for Command Line Parameters for color
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--color', required=False)
-    args = parser.parse_args()
-
-    if args.color:
-        print("Color from command line argument =" + args.color)
-        COLOR = args.color
-        if COLOR_FROM_ENV:
-            print("A color was set through environment variable -" + COLOR_FROM_ENV + ". However, color from command line argument takes precendence.")
-    elif COLOR_FROM_ENV:
-        print("No Command line argument. Color from environment variable =" + COLOR_FROM_ENV)
-        COLOR = COLOR_FROM_ENV
+def application(environ, start_response):
+    path = environ['PATH_INFO']
+    method = environ['REQUEST_METHOD']
+    if method == 'POST':
+        try:
+            if path == '/':
+                request_body_size = int(environ['CONTENT_LENGTH'])
+                request_body = environ['wsgi.input'].read(request_body_size)
+                logger.info("Received message: %s" % request_body)
+            elif path == '/scheduled':
+                logger.info("Received task %s scheduled at %s", environ['HTTP_X_AWS_SQSD_TASKNAME'],
+                            environ['HTTP_X_AWS_SQSD_SCHEDULED_AT'])
+        except (TypeError, ValueError):
+            logger.warning('Error retrieving request body for async work.')
+        response = ''
     else:
-        print("No command line argument or environment variable. Picking a Random Color =" + COLOR)
-
-    # Check if input color is a supported one
-    if COLOR not in color_codes:
-        print("Color not supported. Received '" + COLOR + "' expected one of " + SUPPORTED_COLORS)
-        exit(1)
-
-    # Run Flask Application
-    app.run(host="0.0.0.0",port=8000)
+        response = welcome
+    start_response("200 OK", [
+        ("Content-Type", "text/html"),
+        ("Content-Length", str(len(response)))
+    ])
+    return [bytes(response, 'utf-8')]
