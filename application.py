c4575c730d4ba0d812811ae4a3a1a5ede973ac3f
Jenish Revaldo <129158780+JenishRevaldo@users.noreply.github.com>
2023-09-19 15:55:10 +0530

    Update application.py
    changed html code

--- application.py
+++ application.py
@@ -19,110 +19,92 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 
 welcome = """
-<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-  <!--
-    Copyright 2012 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Jenish Revaldo C - Data Analyst Resume</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            margin: 20px;
+            padding: 20px;
+        }
 
-    Licensed under the Apache License, Version 2.0 (the "License"). You may not use this file except in compliance with the License. A copy of the License is located at
+        h1 {
+            font-size: 28px;
+        }
 
-        http://aws.Amazon/apache2.0/
+        h2 {
+            font-size: 24px;
+            margin-top: 10px;
+        }
 
-    or in the "license" file accompanying this file. This file is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for the specific language governing permissions and limitations under the License.
-  -->
-  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-  <title>Welcome</title>
-  <style>
-  body {
-    color: #ffffff;
-    background-color: #E0E0E0;
-    font-family: Arial, sans-serif;
-    font-size:14px;
-    -moz-transition-property: text-shadow;
-    -moz-transition-duration: 4s;
-    -webkit-transition-property: text-shadow;
-    -webkit-transition-duration: 4s;
-    text-shadow: none;
-  }
-  body.blurry {
-    -moz-transition-property: text-shadow;
-    -moz-transition-duration: 4s;
-    -webkit-transition-property: text-shadow;
-    -webkit-transition-duration: 4s;
-    text-shadow: #fff 0px 0px 25px;
-  }
-  a {
-    color: #0188cc;
-  }
-  .textColumn, .linksColumn {
-    padding: 2em;
-  }
-  .textColumn {
-    position: absolute;
-    top: 0px;
-    right: 50%;
-    bottom: 0px;
-    left: 0px;
+        h3 {
+            font-size: 20px;
+            margin-top: 10px;
+        }
 
-    text-align: right;
-    padding-top: 11em;
-    background-color: #1BA86D;
-    background-image: -moz-radial-gradient(left top, circle, #6AF9BD 0%, #00B386 60%);
-    background-image: -webkit-gradient(radial, 0 0, 1, 0 0, 500, from(#6AF9BD), to(#00B386));
-  }
-  .textColumn p {
-    width: 75%;
-    float:right;
-  }
-  .linksColumn {
-    position: absolute;
-    top:0px;
-    right: 0px;
-    bottom: 0px;
-    left: 50%;
+        p {
+            margin: 5px 0;
+        }
 
-    background-color: #E0E0E0;
-  }
+        .contact {
+            margin-top: 20px;
+        }
 
-  h1 {
-    font-size: 500%;
-    font-weight: normal;
-    margin-bottom: 0em;
-  }
-  h2 {
-    font-size: 200%;
-    font-weight: normal;
-    margin-bottom: 0em;
-  }
-  ul {
-    padding-left: 1em;
-    margin: 0px;
-  }
-  li {
-    margin: 1em 0em;
-  }
-  </style>
+        .education, .projects, .languages, .interests {
+            margin-top: 30px;
+        }
+    </style>
 </head>
-<body id="sample">
-  <div class="textColumn">
-    <h1>Congratulations Jenish</h1>
-    <p>Your first AWS Elastic Beanstalk Python Application is now running on your own dedicated environment in the AWS Cloud</p>
-    <p>This environment is launched with Elastic Beanstalk Python Platform</p>
-  </div>
- 
-  <div class="linksColumn">
-    <h2>What's Next?</h2>
-    <ul>
-    <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/">AWS Elastic Beanstalk overview</a></li>
-    <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/index.html?concepts.html">AWS Elastic Beanstalk concepts</a></li>
-    <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/create_deploy_Python_django.html">Deploy a Django Application to AWS Elastic Beanstalk</a></li>
-    <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/create_deploy_Python_flask.html">Deploy a Flask Application to AWS Elastic Beanstalk</a></li>
-    <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/create_deploy_Python_custom_container.html">Customizing and Configuring a Python Container</a></li>
-    <li><a href="http://docs.amazonwebservices.com/elasticbeanstalk/latest/dg/using-features.loggingS3.title.html">Working with Logs</a></li>
+<body>
+    <h1>Jenish Revaldo C   -  Data Analyst</h1>
+  
+    <p>Driven and adaptable Data Analyst with a passion for continuous learning and leadership. Skilled in mathematics, statistics, and programming, I excel at solving complex problems and extracting valuable insights from data. Committed to staying updated on industry trends to deliver impactful data-driven solutions.</p>
+    
+    <div class="contact">
+        <h2>Contact Information</h2>
+        <p>Email: jenishrev0603@gmail.com</p>
+        <p>Phone: 7358941365</p>
+        <p>Location: Kanyakumari, India</p>
+        <p>LinkedIn: <a href="https://www.linkedin.com/in/jenish-revaldo-c-004670245">linkedin.com/in/jenish-revaldo-c-004670245</a></p>
+        <p>GitHub: <a href="https://github.com/JenishRevaldo">github.com/JenishRevaldo</a></p>
+    </div>
 
-    </ul>
-  </div>
+    <div class="education">
+        <h2>Education</h2>
+        <h3>B.Sc. Mathematics</h3>
+        <p>Loyola College</p>
+        <p>06/2019 - 04/2022, Chennai</p>
+        <p>Graduate with a first-class degree.</p>
+
+        <h3>M.Sc. Data Science</h3>
+        <p>Loyola College</p>
+        <p>06/2022 - Present, Chennai</p>
+        <p>Pursuing degree in data Science with a commendable CGPA.</p>
+    </div>
+
+    <div class="projects">
+        <h2>Projects</h2>
+       <p>IPL Analysis through Power BI</p>
+        <p>Supervised-Unsupervised integration for Breast Cancer diagnosis</p>
+    </div>
+
+    <div class="languages">
+        <h2>Languages</h2>
+        <p>Tamil - Native or Bilingual Proficiency</p>
+        <p>English - Full Professional Proficiency</p>
+    </div>
+
+    <div class="interests">
+        <h2>Interests</h2>
+        <p>Data Analysis</p>
+        <p>Predictive Modelling</p>
+        <p>Natural Language Processing</p>
+        <p>Data Visualization</p>
+    </div>
 </body>
 </html>
 """
