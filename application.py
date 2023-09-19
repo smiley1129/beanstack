0ac6619277948e530d54248ed1ae2cd69678dd59
Jenish Revaldo <129158780+JenishRevaldo@users.noreply.github.com>
2023-09-19 11:39:58 +0530

    Update application.py

--- application.py
+++ application.py
@@ -19,7 +19,7 @@ color_codes = {
 SUPPORTED_COLORS = ",".join(color_codes.keys())
 
 # Get color from Environment variable
-COLOR_FROM_ENV = os.environ.get('APP_COLOR')
+COLOR_FROM_ENV = "blue"
 # Generate a random color
 COLOR = random.choice(["red", "green", "blue", "blue2", "darkblue", "pink"])
 
