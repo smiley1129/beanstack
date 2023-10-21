3b0b369da820cfe7d7b30250c8eb9863650f7dab
SOUNDARI S <129158995+smiley1129@users.noreply.github.com>
2023-10-21 10:20:20 +0530

    Update application.py

--- application.py
+++ application.py
@@ -24,7 +24,7 @@ welcome = """
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jenish Revaldo C - Data Analyst Resume</title>
+    <title>Smiley - Data Analyst Resume</title>
     <style>
         body {
             font-family: Arial, sans-serif;
